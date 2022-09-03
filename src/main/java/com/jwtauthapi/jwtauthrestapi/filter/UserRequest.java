10a30a02055ed4dce7149c08ea33c6ecee31f7d5
dayyanali <dayyanali99@gmail.com>
2022-09-03 12:55:44 +0500

    CORS enable

---
+++ src/main/java/com/jwtauthapi/jwtauthrestapi/filter/UserRequest.java
@@ -0,0 +1,13 @@
+package com.jwtauthapi.jwtauthrestapi.filter;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserRequest {
+    private String username;
+    private String password;
+}
